3aca0034857c1de9370d5a0fc0e3f8315e6cf32b
kimyeong-chae <rdc3884@gmail.com>
2018-11-23 01:06:58 +0900

    블로그 숙제 부분 뺀 나머지 커밋

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Form.css'
+
+
+export default class Form extends Component {
+
+    render() {
+        return (
+            <div className="form">
+                <input value={this.props.value} onChange={this.props.onChange} onKeyPress={this.props.onKeyPress} />
+                <div className="create-button" onClick={this.props.onCreate}>
+                    추가
+                </div>
+            </div>
+        );
+    }
+
+}
--- src/components/TodoListTemplate.js
+++ src/components/TodoListTemplate.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './TodoListTemplate.css';
 
 const TodoListTemplate = ({form,children}) => {
